3485b93e9eb482b5630b14d4707edcb1da69eeca
martial <martial.kom@abyster.com>
2024-02-23 11:41:26 +0100

    Gestion des chemins inexistants

--- src/server.js
+++ src/server.js
@@ -51,6 +51,14 @@ app.use(bodyParser.json());
 
 //Rediriger tous les path "/auth"
 app.use("/auth", require("./routes/authRoutes"));
+
+//Rediriger tous les path "/"
 app.use("/", require("./routes/userRoutes"));
+
+// Middleware pour gérer les requêtes vers des chemins inexistants
+app.use((req, res, next) => {
+    res.status(404).sendFile(racineProjet+'/src/views/pages/notFound.html');
+  });
+
 //Lancer le serveur
 app.listen(port, ()=> console.log("Le serveur a demarré au port: "+port))
